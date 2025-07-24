c7000508866b6802e47cc87ba9b234c76f45bbaa
Mikaela <sambomikaela@gmail.com>
2025-07-24 20:36:23 +0200

    collegato tabelle liked_products e searches a models

--- mysql.sql
+++ mysql.sql
@@ -46,6 +46,8 @@ CREATE TABLE `liked_products` (
   `ID` varchar(255) NOT NULL,
   `timestamp` datetime NOT NULL,
   PRIMARY KEY (`user_email`,`ID`),
+  KEY `fk_liked_products_product` (`ID`),
+  CONSTRAINT `fk_liked_products_product` FOREIGN KEY (`ID`) REFERENCES `models` (`id`) ON DELETE CASCADE,
   CONSTRAINT `liked_products_ibfk_1` FOREIGN KEY (`user_email`) REFERENCES `users` (`email`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -109,8 +111,10 @@ CREATE TABLE `searches` (
   `timestamp` datetime NOT NULL,
   PRIMARY KEY (`ID`),
   UNIQUE KEY `user_email` (`user_email`,`product_id`,`timestamp`),
+  KEY `fk_searches_product` (`product_id`),
+  CONSTRAINT `fk_searches_product` FOREIGN KEY (`product_id`) REFERENCES `models` (`id`) ON DELETE CASCADE,
   CONSTRAINT `searches_ibfk_1` FOREIGN KEY (`user_email`) REFERENCES `users` (`email`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=263 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -157,4 +161,4 @@ CREATE TABLE `users` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-07-22 11:58:27
+-- Dump completed on 2025-07-24 17:29:45
