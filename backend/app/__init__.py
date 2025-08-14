18d22defab788e1ac09b4f004af1152a1ee9e0c0
Sergio <sergmose@gmail.com>
2025-08-14 12:14:29 +0200

    fix: added into init the jwt config

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,25 +1,30 @@
 from flask import Flask
 from flask_mail import Mail
+from flask_jwt_extended import JWTManager
 from dotenv import load_dotenv
-import os
+import os, secrets
 
 #from .routes.views import views_bp
 from .routes.products import products_bp
 from .routes.auth import auth_bp
 
-mail = Mail()
+# --- CREAZIONE APP ---
 app = Flask(__name__)
-
 load_dotenv()  # carica le variabili dal file .env
 
-# --- INIT MAIL ---
+# --- CONFIG MAIL ---
 app.config['MAIL_SERVER'] = os.getenv('MAIL_SERVER')
 app.config['MAIL_PORT'] = int(os.getenv('MAIL_PORT'))
 app.config['MAIL_USE_TLS'] = os.getenv('MAIL_USE_TLS') == 'True'
 app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
 
+mail = Mail()
 mail.init_app(app)  # inizializza Mail
+
+# --- CONFIG JWT ---
+app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY', secrets.token_hex(32))
+jwt = JWTManager(app)
 
 # --- BLUEPRINT ---
 app.register_blueprint(products_bp)
