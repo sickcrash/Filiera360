6580a8460e1feb7dd87b710ef5026a640a559f2c
Sergio <sergmose@gmail.com>
2025-08-29 09:50:27 +0200

    fix: removed use_reloader=false

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -1,14 +1,4 @@
 from app import app
 
-"""
-debug = true -> reload = true
-Flask crea due processi all’avvio:
-    Uno è il monitor che osserva i file.
-    L’altro è il processo reale che esegue la tua app.
-    
-    Siccome ho usato i threadPool globali, ne avrai 2. 
-    Per perforamnce reali, disattivare il reload -> use_reloader=False
-"""
-
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True, use_reloader=False)
+    app.run(host="0.0.0.0", port=5000, debug=True)
